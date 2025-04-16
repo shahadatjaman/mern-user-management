f00d2637b2255c50c0d2a258672d4d8368b6fa54
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-16 22:30:19 +0600

    Add User schema

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      minlength: 2
+    },
+    avatar: {
+        type: String,
+        required: [true, 'Avatar is required'],
+        trim: true,
+        minlength: 2
+      },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid email']
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: 6
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin', 'manager'],
+      default: 'user'
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
