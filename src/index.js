18cd32fb952622d0e41db5bf38000ad338f061e0
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-17 11:36:48 +0600

    feat: add Multer error handling middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const multer = require('multer');
+
 
 const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/authRoutes');
+const AppError = require('./utils/AppError');
+
 dotenv.config();
+
 const app = express();
 
 app.use(express.json());
@@ -20,6 +26,20 @@ app.get('/', (req, res) => {
     res.send('🎉 User Management API is running...');
   });
   
+
+// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use('/uploads', express.static('uploads'));
+
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(err.statusCode || 400).json({ message: err.message });
+  }
+  next(err);
+});
+
+
 connectDB();
 
 const PORT = process.env.PORT || 5000;
