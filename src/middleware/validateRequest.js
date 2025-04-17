61a29c84c967d2665abdc56d9f7aff9a85d21c88
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-17 11:28:22 +0600

    feat: add request validation middleware using express-validator

---
+++ src/middleware/validateRequest.js
@@ -0,0 +1,13 @@
+const { validationResult } = require('express-validator');
+
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ errors: errors.array().map(err => err.msg) });
+  }
+  next();
+};
+
+module.exports = validateRequest;
