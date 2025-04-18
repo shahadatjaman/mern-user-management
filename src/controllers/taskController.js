8a0d65cdcf8e5d0428d295207f28c5edc8536e1e
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 18:14:13 +0600

    refactor(task-route): check validity of project id and remove redundancy debug code

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,12 +1,14 @@
+const { Types, isValidObjectId } = require('mongoose');
 const Task = require('../models/Task');
 
 exports.createTask = async (req, res) => {
   try {
+    console.log('req.body',req.body);
     const { title, description, projectId, dueDate, assignedTo } = req.body;
     const task = await Task.create({
       title,
       description,
-      project: projectId,
+      project: new Types.ObjectId(projectId),
       dueDate,
       assignedTo,
       createdBy: req.user.id
@@ -19,7 +21,15 @@ exports.createTask = async (req, res) => {
 
 exports.getProjectTasks = async (req, res) => {
   try {
-    const tasks = await Task.find({ project: req.params.projectId });
+    
+    if(!isValidObjectId(req.params.id)){
+     return res.status(400).json({message:"Invalid ObjectId"});
+    };
+
+    const projectId = new Types.ObjectId(req.params.id);
+    const tasks = await Task.find({ project: projectId });
+    
+
     res.status(200).json(tasks);
   } catch (error) {
     res.status(500).json({ message: 'Fetching tasks failed', error });
