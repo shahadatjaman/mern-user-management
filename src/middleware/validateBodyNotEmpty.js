7d6285fc0d9ee9b63f20796318eaf893dc280088
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 16:48:52 +0600

    feat: add reusable middleware to reject empty request bodies

---
+++ src/middleware/validateBodyNotEmpty.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        error: 'Request body cannot be empty',
+      });
+    }
+    next();
+  };
+  
