357b78b1d70eec9a4942c8836ec054c9a54c2071
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 16:49:23 +0600

    feat: add authentication and validation middleware for task routes; restrict access by role

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -7,12 +7,13 @@ const {
 
 } = require('../controllers/taskController');
 const { createTaskValidator } = require('../validators/taskValidator');
+const validateBodyNotEmpty = require('../middleware/validateBodyNotEmpty');
 
 const router = express.Router();
 
 router.use(protect); // All routes below this require authentication
 
-router.post('/',restrictTo('admin','manager','member'), createTaskValidator,createTask);
+router.post('/',restrictTo('admin','manager','member'), validateBodyNotEmpty,createTaskValidator,createTask);
 router.get('/project/:projectId', restrictTo('admin','manager','member'),getProjectTasks);
 
 module.exports = router;
