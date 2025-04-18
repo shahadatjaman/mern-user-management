7ff3b7f36681ebd680cb690118de470653abd034
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 16:57:18 +0600

    feat: add middleware to check if request body is empty

--- src/middleware/validateBodyNotEmpty.js
+++ src/middleware/validateBodyNotEmpty.js
@@ -1,9 +1,10 @@
 module.exports = (req, res, next) => {
-    if (!req.body || Object.keys(req.body).length === 0) {
+
+    if (!req.body) {
       return res.status(400).json({
         error: 'Request body cannot be empty',
       });
     }
+
     next();
-  };
-  
+};
