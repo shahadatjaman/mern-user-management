449a8f903fbae6ecb75a063785ae1e13fd6eb8da
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 18:28:53 +0600

    feature(subtask-controller): implement getSubtast,updateTask and deleteTask in controller

--- src/controllers/subtaskController.js
+++ src/controllers/subtaskController.js
@@ -1,3 +1,4 @@
+const { isValidObjectId } = require('mongoose');
 const Subtask = require('../models/Subtask');
 
 exports.createSubtask = async (req, res) => {
@@ -7,5 +8,58 @@ exports.createSubtask = async (req, res) => {
     res.status(201).json(subtask);
   } catch (error) {
     res.status(500).json({ message: 'Creating subtask failed', error });
+  }
+};
+exports.getSubtasksByTask = async (req, res) => {
+  try {
+
+    if(!isValidObjectId(req.params.id)){
+      return res.status(400).json({message:"Invalid ObjectId"});
+    };
+
+    const taskId = new Types.ObjectId(req.params.taskId);
+
+    const subtasks = await Subtask.find({ task: taskId }).populate('createdBy', 'name email');
+    res.json(subtasks);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to fetch subtasks', error: err.message });
+  }
+};
+
+exports.updateSubtask = async (req, res) => {
+  try {
+
+
+    if(!isValidObjectId(req.params.id)){
+      return res.status(400).json({message:"Invalid ObjectId"});
+    };
+
+    const id = new Types.ObjectId(req.params.id);
+
+    const subtask = await Subtask.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!subtask) return res.status(404).json({ message: 'Subtask not found' });
+    res.json(subtask);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to update subtask', error: err.message });
+  }
+};
+
+exports.deleteSubtask = async (req, res) => {
+  try {
+
+    if(!isValidObjectId(req.params.id)){
+      return res.status(400).json({message:"Invalid ObjectId"});
+    };
+
+    const id = new Types.ObjectId(req.params.id);
+    const subtask = await Subtask.findByIdAndDelete(id);
+    if (!subtask) return res.status(404).json({ message: 'Subtask not found' });
+    res.json({ message: 'Subtask deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete subtask', error: err.message });
   }
 };
