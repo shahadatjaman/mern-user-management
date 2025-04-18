c9e11195aea7d844ce3d660f48508dcee9894e6a
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 17:16:28 +0600

    feat(routes): add middleware to check req body is not empty

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.use(protect); // All routes below this require authentication
 
-router.post('/',restrictTo('admin','manager','member'), validateBodyNotEmpty,createTaskValidator,createTask);
+router.post('/',restrictTo('admin','manager','member'),validateBodyNotEmpty,createTaskValidator,createTask);
 router.get('/project/:projectId', restrictTo('admin','manager','member'),getProjectTasks);
 
 module.exports = router;
