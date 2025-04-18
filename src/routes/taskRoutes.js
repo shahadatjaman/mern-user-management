3fd28cf776c1495828d481760bf15e73f979efa5
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 17:34:49 +0600

    feat(task-router): add validationRequest middleware

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -8,12 +8,13 @@ const {
 } = require('../controllers/taskController');
 const { createTaskValidator } = require('../validators/taskValidator');
 const validateBodyNotEmpty = require('../middleware/validateBodyNotEmpty');
+const validateRequest = require('../middleware/validateRequest');
 
 const router = express.Router();
 
 router.use(protect); // All routes below this require authentication
 
-router.post('/',restrictTo('admin','manager','member'),validateBodyNotEmpty,createTaskValidator,createTask);
+router.post('/',restrictTo('admin','manager','member'),validateBodyNotEmpty,createTaskValidator,validateRequest,createTask);
 router.get('/project/:projectId', restrictTo('admin','manager','member'),getProjectTasks);
 
 module.exports = router;
