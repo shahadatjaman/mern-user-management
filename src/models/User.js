b088204e9980ed7807da0a592ba7713ccd730dfe
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-18 11:36:45 +0600

    refactor(user-model): improve password hashing pre-save

--- src/models/User.js
+++ src/models/User.js
@@ -32,4 +32,16 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// 🔐 Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// 🔍 Compare password method
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 module.exports = mongoose.model('User', userSchema);
