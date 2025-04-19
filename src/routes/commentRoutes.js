931b62116c0d9842f21c6139f20ff752732acd8d
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-19 16:11:39 +0600

    feat(routers): add validation middleware to creating commnets

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -8,13 +8,14 @@ const {
   updateComment,
   deleteComment
 } = require('../controllers/commentController');
+const validateComment = require('../validators/validateComment');
 
 const router = express.Router();
 
 router.use(protect); // All routes below this require authentication
 
 
-router.post('/', restrictTo('admin','manager','member'),createComment);                         // Create comment
+router.post('/', restrictTo('admin','manager','member'),validateComment, createComment);          // Create comment
 router.get('/task/:taskId', restrictTo('admin','manager','member'), getCommentsByTask);          // Get comments by task
 router.put('/:id', restrictTo('admin','manager','member'), updateComment);                       // Update comment
 router.delete('/:id', restrictTo('admin','manager','member'),deleteComment);                    // Delete comment
