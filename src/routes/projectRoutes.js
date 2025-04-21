34e611f036cba1c827f48832d1076b527ab41e71
Shahadat Jaman <devloperabuhuraira@gmail.com>
2025-04-21 16:01:00 +0600

    feat(routes): integrated all routes in project.routes.js with authentication middleware

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const { protect, restrictTo } = require('../middleware/authMiddleware');
+const express = require("express");
+const { protect, restrictTo } = require("../middleware/authMiddleware");
 
 const {
   createProject,
   getUserProjects,
-
-} = require('../controllers/projectController');
-const validateProject = require('../middleware/validateProject');
+  getSingleProject,
+  updateProject,
+  deleteProject,
+} = require("../controllers/projectController");
+const validateProject = require("../middleware/validateProject");
+const validateProjectUpdate = require("../middleware/validateProjectUpdate");
 
 const router = express.Router();
 
 router.use(protect); // All routes below this require authentication
 
-
-router.post('/',restrictTo('admin','manager','member'), validateProject,createProject);
-router.get('/',restrictTo('admin','manager','member'), getUserProjects);
-
+router.post(
+  "/",
+  restrictTo("admin", "manager", "member"),
+  validateProject,
+  createProject
+);
+router.get("/", restrictTo("admin", "manager", "member"), getUserProjects);
+router.get("/:id", restrictTo("admin", "manager", "member"), getSingleProject);
+router.put(
+  "/:id",
+  restrictTo("admin", "manager", "member"),
+  validateProjectUpdate,
+  updateProject
+);
+router.delete("/:id", restrictTo("admin", "manager", "member"), deleteProject);
 
 module.exports = router;
